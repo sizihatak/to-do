3c82df5a51a1c9fd47f51e92ca3281964e692892
Alyokhin Denis <da@anahoret.com>
2017-09-19 13:47:08 +0300

    Init commit.

---
+++ app/src/main/kotlin/com/example/todo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todo
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+    }
+}
